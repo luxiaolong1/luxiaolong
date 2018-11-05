b19ae3ddca9d0cff6df133b65d2092b577705bc7
luxiaolong1 <1459242070@qq.com>
2018-11-05 17:37:05 +0800

    axf

---
+++ Django1/Axf/models.py
@@ -0,0 +1,106 @@
+from django.db import models
+
+# Create your models here.
+# 基础 类
+class Base(models.Model):
+    img = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    trackid = models.CharField(max_length=20)
+
+    class Meta:
+        abstract = True
+    def __str__(self):
+        return self.name
+
+# 轮播图 模型类
+class Wheel(Base):
+    class Meta:
+        db_table = 'axf_wheel'
+# 导航 模型类
+class Nav(Base):
+    class Meta:
+        db_table = 'axf_nav'
+
+# 每日必购 模型类
+class Mustbuy(Base):
+    class Meta:
+        db_table = 'axf_mustbuy'
+
+# 商品部分
+class Shop(Base):
+    class Meta:
+        db_table = 'axf_shop'
+
+
+
+# 商品主体内容
+# (trackid,name,img,categoryid,brandname,
+# img1,childcid1,productid1,longname1,price1,marketprice1,img2,childcid2,productid2,longname2,price2,marketprice2,img3,childcid3,productid3,longname3,price3,marketprice3)
+
+# values("21782","优选水果","http://img01.bqstatic.com//upload/activity/2017031018205492.jpg@90Q.jpg","103532","爱鲜蜂",
+
+# "http://img01.bqstatic.com/upload/goods/201/701/1916/20170119164159_996462.jpg@200w_200h_90Q","103533","118824","爱鲜蜂·特小凤西瓜1.5-2.5kg/粒","25.80","25.8","http://img01.bqstatic.com/upload/goods/201/611/1617/20161116173544_219028.jpg@200w_200h_90Q","103534","116950","蜂觅·越南直采红心火龙果350-450g/盒","15.3","15.8","http://img01.bqstatic.com/upload/goods/201/701/1916/20170119164119_550363.jpg@200w_200h_90Q","103533","118826","爱鲜蜂·海南千禧果400-450g/盒","9.9","13.8");
+class MainShow(models.Model):
+    trackid = models.CharField(max_length=8)
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=100)
+    categoryid = models.CharField(max_length=8)
+    brandname = models.CharField(max_length=50)
+
+    img1 = models.CharField(max_length=100)
+    childcid1 = models.CharField(max_length=8)
+    productid1 = models.CharField(max_length=8)
+    longname1 = models.CharField(max_length=100)
+    price1 = models.FloatField()
+    marketprice1 = models.FloatField()
+
+    img2 = models.CharField(max_length=100)
+    childcid2 = models.CharField(max_length=8)
+    productid2 = models.CharField(max_length=8)
+    longname2 = models.CharField(max_length=100)
+    price2 = models.FloatField()
+    marketprice2 = models.FloatField()
+
+    img3 = models.CharField(max_length=100)
+    childcid3 = models.CharField(max_length=8)
+    productid3 = models.CharField(max_length=8)
+    longname3 = models.CharField(max_length=100)
+    price3 = models.FloatField()
+    marketprice3 = models.FloatField()
+
+    class Meta:
+        db_table = 'axf_mainshow'
+
+
+# 商品分类
+# insert into axf_foodtypes(typeid,typename,childtypenames,typesort)
+class Foodtypes(models.Model):
+    typeid = models.CharField(max_length=8) # 分类ID
+    typename = models.CharField(max_length=100) # 分类名称
+    childtypenames = models.CharField(max_length=256)   # 子类名称
+    typesort = models.IntegerField()    # 显示先后顺序
+    class Meta:
+        db_table = 'axf_foodtypes'
+
+
+# 商品信息
+class Goods(models.Model):
+    productid = models.CharField(max_length=10) # 商品ID
+    productimg = models.CharField(max_length=100)   # 商品图片
+    productname = models.CharField(max_length=100)  # 商品名称
+    productlongname = models.CharField(max_length=100)  # 商品弄名称
+    isxf = models.BooleanField(default=False)    # 精选
+    pmdesc = models.BooleanField(default=False)  # 买一送一
+    specifics = models.CharField(max_length=100)   # 规格
+    price = models.DecimalField(max_digits=7, decimal_places=2)    # 价格
+    marketprice = models.DecimalField(max_digits=7, decimal_places=2)  # 商场价格
+    categoryid= models.IntegerField()   # 分类ID
+    childcid = models.IntegerField()    # 子类ID
+    childcidname = models.CharField(max_length=100) # 分类名称
+    dealerid = models.CharField(max_length=10)  # 详情ID
+    storenums = models.IntegerField()   # 库存
+    productnum = models.IntegerField()  # 销量
+
+    class Meta:
+        db_table = 'axf_goods'
+
